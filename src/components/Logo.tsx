0691aff93b1c7cc4be690183dd7ec951b95df2bc
Eugene Zorin <ezorin5@yandex.ru>
2023-09-13 16:50:14 +0400

    logo redesign

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,16 +9,16 @@ const Logo: FC = () => {
     return (
         <Link
             href={routesConfig.getMainRoute()}
-            className={`${utilsStyles['flex-row']} ${utilsStyles['align-center']} ${utilsStyles['gap-xxs']}`}
+            className={`${utilsStyles['flex-row']} ${utilsStyles['align-center']} ${utilsStyles['gap-xxs']} ${styles.logo}`}
         >
-            <span className={styles.logo}>space</span>
+            <span className={styles.text}>space</span>
             <Image
                 src={logoIconConfig.src}
                 alt={logoIconConfig.alt}
                 width={50}
                 height={50}
             />
-            <span className={styles.logo}>monitor</span>
+            <span className={styles.text}>monitor</span>
         </Link>
     );
 };
